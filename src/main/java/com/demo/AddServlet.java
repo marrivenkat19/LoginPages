998aa2b71ecf194b4d80406df2ee6f27e0182cdb
marrivenkat19 <venkateswarareddy.marri@wavelabs.ai>
2021-11-15 16:08:59 +0530

    login pages project

---
+++ src/main/java/com/demo/AddServlet.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet  {
+
+	public void service(HttpServletRequest req , HttpServletResponse res) throws IOException {
+		
+		String user = req.getParameter("user name");
+		String password =  req.getParameter("password");
+		
+		
+		PrintWriter out = res.getWriter();
+		 
+		/*if (user.equals("user") && password.equals("password"))
+			out.println("Login Success...!");
+		else out.println("Login Failed...!");*/ 
+		
+		
+		
+		 
+		
+		
+		 out.println("LOGIN SUCUSESS ! welcome venkat reddy " );
+		 out.close();
+		// out.println("LOGIN SUCCUSES !  WELCOME VENKAT REDDY : " );
+		
+	}
+
+}
